dccfda51f554bb9ad4da4fb12d6768d76c70f14b
Daniel Skinner <daniel@dasa.cc>
2014-08-30 18:55:37 -0500

    cleanup shaders

--- rocky-frag.glsl
+++ rocky-frag.glsl
@@ -1,27 +1,16 @@
 #version 130
 
-in vec3 FragPos;
-in float ao;
-
 uniform float time;
 
 varying vec2 tc0;
-varying float tiles0;
 uniform sampler2D Texture0;
 
-void main()
-{	
-	vec3 normal = vec3(1, 1, 1);
-	vec3 pos = vec3(0, 3, 0);
-	vec3 surfaceToLight = pos - FragPos;
-	float brightness = dot(normal, surfaceToLight) / (length(surfaceToLight) * length(normal));
-  brightness = clamp(brightness, 0, 1);
-  brightness *= 4.5;
-	vec3 light = brightness * vec3(0.9, 0.8, 0.7);
+varying float tiles0;
 
+void main() {
 	vec4 final = vec4(0, 0, 0, 0);
 	vec4 t0 = texture2D(Texture0, tc0 * tiles0);
 	final = mix(final, t0, t0.a);
 
-	gl_FragColor = vec4(light, 1.0) * ao * final;
+  gl_FragColor = gl_Color * final;
 }
--- rocky-vert.glsl
+++ rocky-vert.glsl
@@ -1,40 +1,64 @@
 #version 130
 
 attribute vec4 Vertex;
-attribute vec4 Color;
-attribute vec2 TexCoord0;
-
-varying vec2 tc0;
-
-uniform float time;
-
-out vec3 FragPos;
-out float ao;
-out float tiles0;
-
-uniform mat4 Projection;
 uniform mat4 MVP;
+
+attribute vec2 TexCoord0;
+varying vec2 tc0;
 
 uniform sampler2D Texture0;
 
-void main()
-{
-	FragPos = vec3(Vertex);
-	tc0 = TexCoord0;
+uniform float time;
+
+out float tiles0;
+
+void main() {
 	tiles0 = 1;
-	
+	tc0 = TexCoord0;
+
+	// sample texture to deform mesh
 	vec4 t0 = texture2D(Texture0, tc0 * tiles0);
 	float offset = normalize(t0).x;
-	// motion sickness
-	// offset += cos(time) * cos(time);
-	
 	// use offset as cheap ambient occlusion
-	ao = offset;
+	float ao = offset;
+	// motion sickness
+	// offset += cos(time) * sin(time);
 	
 	vec3 center = vec3(0, 0, 0);
-	vec3 pos = vec3(Vertex);
-	vec3 verts = pos + (offset * (pos - center));
+	vec3 fpos = vec3(Vertex);
+	vec3 verts = fpos + (offset * (fpos - center));
 
-	// render deformed
+	// normal
+	// gl_Position = MVP * Vertex;
+
+	// deformed
 	gl_Position = MVP * vec4(verts, 1.0);
+
+	// lighting
+	vec3 vertexPos = vec3(gl_Position);
+	vec3 normalDirection = normalize(gl_NormalMatrix * gl_Normal);
+	vec3 lightPos = vec3(0, 10, 2);
+	vec3 lightDirection = normalize(lightPos - vertexPos);
+
+	//
+	vec4 sceneLight = vec4(0.8, 0.7, 0.6, 1.0);
+	
+	//
+	vec4 amb = vec4(1, 1, 1, 1) * ao;
+
+	//
+	vec4 diffuse = vec4(1, 1, 1, 1);
+	diffuse = diffuse * max(dot(lightDirection, normalDirection), 0.0);
+  diffuse = clamp(diffuse, 0.0, 1.0);     
+
+	//
+	vec3 viewDirection = normalize(-vertexPos);
+	vec3 reflection = normalize(reflect(-lightDirection, normalDirection));
+	float shininess = 2;
+
+	//
+	vec4 lightSpec = vec4(1, 1, 1, 1);
+	vec4 spec = lightSpec * pow(max(0.0, dot(reflection, viewDirection)), shininess);
+	
+	gl_FrontColor = (sceneLight + spec + diffuse + amb) * ao;
 }
